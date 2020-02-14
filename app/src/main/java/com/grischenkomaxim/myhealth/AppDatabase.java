1ed728d336b8bc9931542e30010ae8908e21ae1c
Грищенко Максим <grischenko@nsd.ru>
2020-02-14 17:49:55 +0300

    Добавил анимацию волны

--- app/src/main/java/com/grischenkomaxim/myhealth/AppDatabase.java
+++ app/src/main/java/com/grischenkomaxim/myhealth/AppDatabase.java
@@ -4,7 +4,7 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
-@Database(entities = {Weight.class, BloodPressure.class, Water.class}, version = 2)
+@Database(entities = {Weight.class, BloodPressure.class, Water.class}, version = 2, exportSchema = false)
 @TypeConverters({DateConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract WeightDao weightDao();
--- app/src/main/java/com/grischenkomaxim/myhealth/WaterFragment.java
+++ app/src/main/java/com/grischenkomaxim/myhealth/WaterFragment.java
@@ -1,15 +1,13 @@
 package com.grischenkomaxim.myhealth;
 
-import android.content.Context;
-import android.content.res.Resources;
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.PorterDuff;
+import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.ArcShape;
 import android.graphics.drawable.shapes.PathShape;
-import android.graphics.drawable.shapes.RectShape;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -21,25 +19,31 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.ColorRes;
+import java.util.Date;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import java.util.Date;
-
 
 public class WaterFragment extends Fragment {
-    float y;
-    TextView drink;
-    ImageView iv;
-    int countWaves = 5;
-    int amplitude = 10;
-    Button buttonOk;
-    int value;
+
+    private float y;
+    private TextView drink;
+    private ImageView iv;
+    private int amplitude = 10;
+    private Button buttonOk;
+    private int value;
+    private static final int FRAME_LENGHT = 20;
+    private static final int SHIFT = 20;
+    private static final int ATTENUATION = 10;
+    private static final int COUNT_WAVES = 5;
+    public static final int WAVE_HEIGHT = 300;
+
 
     public WaterFragment(){}
 
+    @SuppressLint("ClickableViewAccessibility")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -48,7 +52,7 @@ public class WaterFragment extends Fragment {
         drink = view.findViewById(R.id.drink);
         iv = view.findViewById(R.id.imageView);ArcShape arc = new ArcShape(0, 360);
 
-        final ShapeDrawable shape = new ShapeDrawable(new PathShape(makeWavePath(iv.getWidth(), iv.getHeight(), 300, countWaves, amplitude), iv.getWidth(), iv.getHeight()));
+        final ShapeDrawable shape = new ShapeDrawable(new PathShape(makeWavePath(iv.getWidth(), iv.getHeight(), WAVE_HEIGHT, COUNT_WAVES, amplitude, SHIFT), iv.getWidth(), iv.getHeight()));
         shape.getPaint().setColor(Color.CYAN);
         shape.getPaint().setStyle(Paint.Style.FILL);
 
@@ -59,13 +63,15 @@ public class WaterFragment extends Fragment {
         iv.setBackground(shape);
       //  iv.getDrawable().setColorFilter(view.getBackground().getColorFilter());
       //  view.setBackgroundColor();
+        AnimationDrawable animationDrawable = makeAnimation(iv.getWidth(), iv.getHeight(), WAVE_HEIGHT, COUNT_WAVES, amplitude, FRAME_LENGHT);
+        animationDrawable.addFrame(shape,10);
 
         iv.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 y = motionEvent.getY();
                 if(motionEvent.getAction() == MotionEvent.ACTION_MOVE || motionEvent.getAction() == MotionEvent.ACTION_DOWN){
-                    final ShapeDrawable shape = new ShapeDrawable(new PathShape(makeWavePath(iv.getWidth(), iv.getHeight(), y, countWaves, amplitude), iv.getWidth(), iv.getHeight()));
+                    final ShapeDrawable shape = new ShapeDrawable(new PathShape(makeWavePath(iv.getWidth(), iv.getHeight(), y, COUNT_WAVES, amplitude, SHIFT), iv.getWidth(), iv.getHeight()));
                     shape.getPaint().setColor(Color.CYAN);
                     iv.setBackground(shape);
                     value= (int) (iv.getHeight()- y) * 500 / iv.getHeight();
@@ -89,8 +95,21 @@ public class WaterFragment extends Fragment {
         return new WaterFragment();
     }
 
+    private  AnimationDrawable makeAnimation(int sizeX, int sizeY, float waveHeight, int countWaves, int amplitude, int frameLenght){
+        AnimationDrawable animationDrawable = new AnimationDrawable();
+        float shift = 0.0F;
+        while (amplitude > 0){
+            final ShapeDrawable shape = new ShapeDrawable(new PathShape(makeWavePath(sizeX, sizeY, waveHeight, countWaves, amplitude, shift), iv.getWidth(), iv.getHeight()));
+            shape.getPaint().setColor(Color.CYAN);
+            shape.getPaint().setStyle(Paint.Style.FILL);
+            animationDrawable.addFrame(shape,frameLenght);
+            amplitude = amplitude - ATTENUATION;
+            shift = shift + SHIFT;
+        }
+        return animationDrawable;
+    }
 
-    private Path makeWavePath(int sizeX, int sizeY, float waveHeight, int countWaves, int amplitude){
+    private Path makeWavePath(int sizeX, int sizeY, float waveHeight, int countWaves, int amplitude, float shift){
         float waveQuarterLength = (float) sizeX / countWaves / 4;
         Path path = new Path();
         path.reset();
@@ -99,8 +118,11 @@ public class WaterFragment extends Fragment {
         path.lineTo(0, sizeY);
         path.lineTo(0, waveHeight);
         for (int i = 0; i < countWaves; i++){
-            path.rQuadTo(waveQuarterLength,amplitude * 2, waveQuarterLength * 2, 0);
-            path.rQuadTo(waveQuarterLength,-amplitude * 2, waveQuarterLength * 2, 0);
+            if (shift >= (waveQuarterLength * 2)){
+                shift = shift - (waveQuarterLength * 2);
+            }
+            path.rQuadTo(waveQuarterLength + shift,amplitude * 2, waveQuarterLength * 2 + shift, 0);
+            path.rQuadTo(waveQuarterLength + shift,-amplitude * 2, waveQuarterLength * 2 + shift, 0);
         }
         path.close();
         return path;
